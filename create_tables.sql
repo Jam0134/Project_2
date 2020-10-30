98950bfe9f1200db3d7577e7966f4af43e95a1c1
James Fox <foxjc3@gmail.com>
2020-10-30 12:34:30 -0400

    Adding SQL files for creating schema as well as exports of tables from postgres

---
+++ create_tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE charachters (
+name VARCHAR,
+height VARCHAR,
+mass VARCHAR,
+hair_color VARCHAR,
+skin_color VARCHAR,
+eye_color VARCHAR,
+birth_year VARCHAR,
+gender VARCHAR,
+homeworld VARCHAR,
+species VARCHAR
+	
+);
+
+CREATE TABLE planets (
+name VARCHAR,
+rotation_period VARCHAR,
+orbital_period VARCHAR,
+diameter VARCHAR,
+climate VARCHAR,
+gravity VARCHAR,
+terrain VARCHAR,
+surface_water VARCHAR,
+population VARCHAR
+
+);
+
+CREATE TABLE species (
+name VARCHAR,
+classification VARCHAR,
+designation VARCHAR,
+average_height VARCHAR,
+skin_colors VARCHAR,
+hair_colors VARCHAR,
+eye_colors VARCHAR,
+average_lifespan VARCHAR,
+language VARCHAR,
+homeworld VARCHAR
+
+);
